173f4f698e556d50b1dfb0a048e23e703eed760b
Scott Hicks <sa.hicks@icloud.com>
2020-03-05 07:32:44 +0000

    Project 7 - Challenge 1
    "Add an Edit/Done button to ContentView so users can delete rows more easily."

--- project7/project7/ContentView.swift
+++ project7/project7/ContentView.swift
@@ -58,7 +58,7 @@ struct ContentView: View {
             .onDelete(perform: removeItems)
             }
         .navigationBarTitle("iExpense")
-        .navigationBarItems(trailing:
+        .navigationBarItems(leading: EditButton(), trailing:
             Button(action: {
                 self.showingAddExpense = true
             }) {
